ce693c6c64b45599fe3e23b81d29ddd148b63cea
chikoulat <hamidouboum@hotmail.fr>
2023-11-22 10:59:04 +0100

    A one day project with the Wild code school to learn more about filter, props and useState method.

---
+++ src/components/filter.jsx
@@ -0,0 +1,54 @@
+import PropTypes from "prop-types";
+import "../assets/styles/filter.css";
+
+function Filter({ setCheckboxHome, setCheckboxApartment, setCheckboxVilla }) {
+  const handleCheckbox = (e) => {
+    setCheckboxHome(e.target.checked);
+  };
+  const handleCheckboxApartment = (e) => {
+    setCheckboxApartment(e.target.checked);
+  };
+  const handleCheckboxVilla = (e) => {
+    setCheckboxVilla(e.target.checked);
+  };
+
+  return (
+    <div className="filter d-flex ">
+      <div>
+        <input
+          onChange={handleCheckboxVilla}
+          type="checkbox"
+          name="villa"
+          id="villa"
+        />
+        <label htmlFor="villa">villa</label>
+      </div>
+      <div>
+        <input
+          onChange={handleCheckboxApartment}
+          type="checkbox"
+          name="apartment"
+          id="apartment"
+        />
+        <label htmlFor="apartment">appartement</label>
+      </div>
+      <div>
+        <input
+          onChange={handleCheckbox}
+          type="checkbox"
+          name="home"
+          id="home"
+        />
+        <label htmlFor="home">maison</label>
+      </div>
+    </div>
+  );
+}
+
+Filter.propTypes = {
+  setCheckboxHome: PropTypes.func.isRequired,
+  setCheckboxApartment: PropTypes.func.isRequired,
+  setCheckboxVilla: PropTypes.func.isRequired,
+};
+
+export default Filter;
